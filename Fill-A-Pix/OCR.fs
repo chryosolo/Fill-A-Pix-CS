b0edc0cc743726fd32585376c96d6d23e3c8241f
Jeff Webster <jwebster@navexglobal.com>
2017-02-08 08:23:30 -0800

    CharTemplates -- load all and create top level matcher

--- Fill-A-Pix/OCR.fs
+++ Fill-A-Pix/OCR.fs
@@ -2,30 +2,40 @@
 
 open System.Drawing
 
-type CharTemplate = 
-    | CharTemplate of Bitmap
+type Char =
+    | Zero = 0
+    | One = 1
+    | Two = 2
+    | Three = 3
+    | Four = 4
+    | Five = 5
+    | Six = 6
+    | Seven = 7
+    | Eight = 8
+    | Nine = 9
 
-type Rgb = byte * byte * byte
+type BlankOrChar = 
+    | Blank
+    | Filled of Char
 
-let colorToRgb (color:Color) =
-    ( color.R, color.G, color.B )
+type CharTemplate = { Char:BlankOrChar; Bitmap:Bitmap }
 
-let calcDiff (rgb1:Rgb) (rgb2:Rgb) =
-    let (r1, g1, b1) = rgb1
-    let (r2, g2, b2) = rgb2
-    let deltaR = float r1 - float r2
-    let deltaG = float g1 - float g2
-    let deltaB = float b1 - float b2
+type Match = BlankOrChar * float
+
+let calcDiff (color1:Color) (color2:Color) =
+    let deltaR = float color1.R - float color2.R
+    let deltaG = float color1.G - float color2.G
+    let deltaB = float color1.B - float color2.B
     sqrt ( deltaR * deltaR + deltaG * deltaG + deltaB * deltaB )
 
-let calculateMatch (template:CharTemplate) (unknown:CharTemplate) : float =
+let calculateMatch (template:Bitmap) (unknown:Bitmap) : float =
     // for now assume the same size
     let scale (tBitmap:Bitmap) (sum:float) =
         sum / float ( tBitmap.Width * tBitmap.Height )
 
     let calcPixelDelta (tBitmap:Bitmap) (uBitmap:Bitmap) x y =
-        let tColor = tBitmap.GetPixel( x, y ) |> colorToRgb
-        let uColor = uBitmap.GetPixel( x, y ) |> colorToRgb
+        let tColor = tBitmap.GetPixel( x, y )
+        let uColor = uBitmap.GetPixel( x, y )
         calcDiff tColor uColor
     
     let calcLineDelta (tBitmap:Bitmap) (uBitmap:Bitmap) y =
@@ -34,11 +44,36 @@ let calculateMatch (template:CharTemplate) (unknown:CharTemplate) : float =
         |> List.map (fun x -> calcPixelDelta tBitmap uBitmap x y )
         |> List.sum
     
-    let (CharTemplate tBitmap) = template
-    let (CharTemplate uBitmap) = unknown
-    let height = tBitmap.Height
+    let height = template.Height
     [0 .. height - 1]
-    |> List.map (fun y -> calcLineDelta tBitmap uBitmap y )
+    |> List.map (fun y -> calcLineDelta template unknown y )
     |> List.sum
-    |> scale tBitmap
+    |> scale template
+
+let loadBitmap name =
+    let filename = sprintf "C:\\git\\Sandbox\\Fill-A-Pix\\Fill-A-Pix\\Numbers\\%s.png" name
+    new Bitmap( Image.FromFile( filename ) )
+
+let blankChar = { Char = Blank; Bitmap = loadBitmap "blank" }
+let zeroChar = { Char = Filled Char.Zero; Bitmap = loadBitmap "0" }
+let oneChar = { Char = Filled Char.One; Bitmap = loadBitmap "1" }
+let twoChar = { Char = Filled Char.Two; Bitmap = loadBitmap "2" }
+let threeChar = { Char = Filled Char.Three; Bitmap = loadBitmap "3" }
+let fourChar = { Char = Filled Char.Four; Bitmap = loadBitmap "4" }
+let fiveChar = { Char = Filled Char.Five; Bitmap = loadBitmap "5" }
+let sixChar = { Char = Filled Char.Six; Bitmap = loadBitmap "6" }
+let sevenChar = { Char = Filled Char.Seven; Bitmap = loadBitmap "7" }
+let eightChar = { Char = Filled Char.Eight; Bitmap = loadBitmap "8" }
+let nineChar = { Char = Filled Char.Nine; Bitmap = loadBitmap "9" }
+let charTemplates = [| blankChar; zeroChar; oneChar; twoChar; threeChar; fourChar; fiveChar; sixChar; sevenChar; eightChar; nineChar |]
+
+let getChar (unknown:Bitmap) =
+    charTemplates
+    |> Array.map ( fun charTemp -> ( charTemp.Char, calculateMatch (charTemp.Bitmap) unknown ) )
+    |> Array.sortBy ( fun charMatch -> snd charMatch )
+    |> Array.take 3
+
+
+
+
 
