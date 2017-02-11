29acf397cca929259c4b54b078c900e8e22d9fb8
Jeff Webster <jwebster@navexglobal.com>
2017-02-10 23:34:22 -0800

    Move game types to Game module, complete OCR capability

---
+++ Fill-A-Pix/Game.fs
@@ -0,0 +1,26 @@
+﻿module Game
+
+type CellState =
+    | Unknown
+    | Filled
+    | Empty
+
+type GivenClue =
+    | Zero = 0
+    | One = 1
+    | Two = 2
+    | Three = 3
+    | Four = 4
+    | Five = 5
+    | Six = 6
+    | Seven = 7
+    | Eight = 8
+    | Nine = 9
+
+type Clue = 
+    | Blank
+    | Given of GivenClue
+
+type Cell = {X:int; Y:int; State:CellState; Clue:Clue}
+
+type Board = {Rows:int; Cols:int; Cells:Cell[]}
--- Fill-A-Pix/OCR.fs
+++ Fill-A-Pix/OCR.fs
@@ -1,27 +1,27 @@
 ﻿module OCR
 
 open System.Drawing
+open Game
 
-type Char =
-    | Zero = 0
-    | One = 1
-    | Two = 2
-    | Three = 3
-    | Four = 4
-    | Five = 5
-    | Six = 6
-    | Seven = 7
-    | Eight = 8
-    | Nine = 9
+type MatchProbability = Clue * float
 
-type BlankOrChar = 
-    | Blank
-    | Filled of Char
+type ClueTemplate = { Clue: Clue; Bitmap: Bitmap }
 
-type CharTemplate = { Char:BlankOrChar; Bitmap:Bitmap }
-
-type MatchProbability = BlankOrChar * float
-
+let clueToString =
+    function
+    | Blank -> " "
+    | Given GivenClue.Zero -> "0"
+    | Given GivenClue.One -> "1"
+    | Given GivenClue.Two -> "2"
+    | Given GivenClue.Three -> "3"
+    | Given GivenClue.Four -> "4"
+    | Given GivenClue.Five -> "5"
+    | Given GivenClue.Six -> "6"
+    | Given GivenClue.Seven -> "7"
+    | Given GivenClue.Eight -> "8"
+    | Given GivenClue.Nine -> "9"
+    | _ -> "X"
+                
 let calcDiff (color1:Color) (color2:Color) =
     let deltaR = float color1.R - float color2.R
     let deltaG = float color1.G - float color2.G
@@ -53,24 +53,24 @@ let loadBitmap name =
     let filename = sprintf "C:\\git\\Sandbox\\Fill-A-Pix\\Fill-A-Pix\\Numbers\\%s.png" name
     new Bitmap( Image.FromFile( filename ) )
 
-let blankChar = { Char = Blank; Bitmap = loadBitmap "blank" }
-let zeroChar = { Char = Filled Char.Zero; Bitmap = loadBitmap "0" }
-let oneChar = { Char = Filled Char.One; Bitmap = loadBitmap "1" }
-let twoChar = { Char = Filled Char.Two; Bitmap = loadBitmap "2" }
-let threeChar = { Char = Filled Char.Three; Bitmap = loadBitmap "3" }
-let fourChar = { Char = Filled Char.Four; Bitmap = loadBitmap "4" }
-let fiveChar = { Char = Filled Char.Five; Bitmap = loadBitmap "5" }
-let sixChar = { Char = Filled Char.Six; Bitmap = loadBitmap "6" }
-let sevenChar = { Char = Filled Char.Seven; Bitmap = loadBitmap "7" }
-let eightChar = { Char = Filled Char.Eight; Bitmap = loadBitmap "8" }
-let nineChar = { Char = Filled Char.Nine; Bitmap = loadBitmap "9" }
-let charTemplates = [| blankChar; zeroChar; oneChar; twoChar; threeChar; fourChar; fiveChar; sixChar; sevenChar; eightChar; nineChar |]
+let blankClue = { Clue = Blank; Bitmap = loadBitmap "blank" }
+let zeroClue = { Clue = Given GivenClue.Zero; Bitmap = loadBitmap "0" }
+let oneClue = { Clue = Given GivenClue.One; Bitmap = loadBitmap "1" }
+let twoClue = { Clue = Given GivenClue.Two; Bitmap = loadBitmap "2" }
+let threeClue = { Clue = Given GivenClue.Three; Bitmap = loadBitmap "3" }
+let fourClue = { Clue = Given GivenClue.Four; Bitmap = loadBitmap "4" }
+let fiveClue = { Clue = Given GivenClue.Five; Bitmap = loadBitmap "5" }
+let sixClue = { Clue = Given GivenClue.Six; Bitmap = loadBitmap "6" }
+let sevenClue = { Clue = Given GivenClue.Seven; Bitmap = loadBitmap "7" }
+let eightClue = { Clue = Given GivenClue.Eight; Bitmap = loadBitmap "8" }
+let nineClue = { Clue = Given GivenClue.Nine; Bitmap = loadBitmap "9" }
+let clueTemplates = [| blankClue; zeroClue; oneClue; twoClue; threeClue; fourClue; fiveClue; sixClue; sevenClue; eightClue; nineClue |]
 
-let getChar (unknown:Bitmap) =
-    charTemplates
-    |> Array.map ( fun charTemp -> ( charTemp.Char, calculateMatch (charTemp.Bitmap) unknown ) )
-    |> Array.sortBy ( fun charMatch -> snd charMatch )
-    |> Array.take 3
+let matchClue (unknown:Bitmap) =
+    clueTemplates
+    |> Array.map ( fun clueTemp -> ( clueTemp.Clue, calculateMatch (clueTemp.Bitmap) unknown ) )
+    |> Array.sortBy ( fun clueMatch -> snd clueMatch )
+    |> Array.head
 
 let rgbMatch (color1:Color) (color2:Color) =
     color1.R = color2.R && color1.G = color2.G && color1.B = color2.B
@@ -86,39 +86,55 @@ let detector (matcher:bool->bool->bool) (color:Color) (offsets:(int*int)[]) (win
     matches
     |> Array.reduce matcher
 
-// from middle of left side, call with increasing X -- returns true if right or above-right pixel is white
 let detectLeftBoundary = detector (||) Color.White [|(1, 0); (1, -1)|]
-
-// from middle of right side, call with decreasing X -- returns true if left or above-left pixel is white
 let detectRightBoundary = detector (||) Color.White [|(-1, 0); (-1, -1)|]
-
-// from middle of bottom, call with decreasing Y -- returns true if above or above-left pixel is white
 let detectBottomBoundary = detector (||) Color.White [|(0, -1); (-1, -1)|]
-
-// from found left boundary (middle Y), call with decreasing Y -- returns true if right AND above-right pixel are gray
 let detectTopLeftCorner = detector (&&) (Color.FromArgb( 132, 132, 132 ) ) [|(1,0); (1,-1)|]
+let detectColumn = detector (&&) (Color.FromArgb( 132, 132, 132 ) ) [|(0,0); (0,1)|]
+let detectRow = detector (&&) (Color.FromArgb( 132, 132, 132 ) ) [|(0,0); (1,0)|]
 
 let findBoard (window:Bitmap) : Bitmap =
     let halfHeight = window.Height/2
     let halfWidth = window.Width/2
     let leftBoundaryX =
-        Seq.skipWhile (fun x -> not ( detectLeftBoundary window x halfHeight ) ) (seq [ 0 .. halfWidth ])
+        Seq.skipWhile (fun x -> not ( detectLeftBoundary window x halfHeight )) (seq [ 0 .. halfWidth ])
         |> Seq.head 
     let bottomBoundaryY =
-        Seq.skipWhile (fun y -> not ( detectBottomBoundary window halfWidth y ) )
+        Seq.skipWhile (fun y -> not ( detectBottomBoundary window halfWidth y ))
                       (seq [ window.Height-1 .. -1 .. halfHeight ])
         |> Seq.head
     let rightBoundaryX =
-        Seq.skipWhile (fun x -> not ( detectRightBoundary window x halfHeight ) )
+        Seq.skipWhile (fun x -> not ( detectRightBoundary window x halfHeight ))
                       (seq [ window.Width-1 .. -1 .. halfWidth ])
         |> Seq.head 
     let topBoundaryY =
-        Seq.skipWhile (fun y -> not ( detectTopLeftCorner window leftBoundaryX y ) )
-                      (seq [ halfHeight .. -1 .. 0 ] )
+        Seq.skipWhile (fun y -> not ( detectTopLeftCorner window leftBoundaryX y ))
+                      (seq [ halfHeight .. -1 .. 0 ])
         |> Seq.head
 
     let boardRect = new Rectangle( leftBoundaryX, topBoundaryY,
                                    rightBoundaryX-leftBoundaryX+1, bottomBoundaryY-topBoundaryY+1 )
-    let board = window.Clone( boardRect, window.PixelFormat )
-    board.Save( "c:\\git\\Sandbox\\Fill-A-Pix\\board.png" )
-    board
+    window.Clone( boardRect, window.PixelFormat )
+
+let findClues (board:Bitmap) =
+    // find grid column pixel boundaries
+    let colBoundaries =
+        [| 0 .. board.Width-1 |]
+        |> Array.where (fun x -> detectColumn board x 0) // x's with vertical lines
+        |> Array.pairwise
+    // find grid row indices and pixel boundaries
+    let rowBoundaries =
+        [| 0 .. board.Height-1 |]
+        |> Array.where (fun y -> detectRow board 0 y)
+        |> Array.pairwise
+    // find grid coords and cell clues
+    Array2D.init (colBoundaries.Length) (rowBoundaries.Length)
+        (fun x y ->
+            let (x0,x1) = colBoundaries.[x]
+            let (y0,y1) = rowBoundaries.[y]
+            let rect = new Rectangle( x0+1, y0+1, x1-x0-1, y1-y0-1 )
+            let bitmap = board.Clone( rect, board.PixelFormat )
+            let (clue,_) = matchClue bitmap
+            clue )
+
+
