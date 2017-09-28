6ccc0dbe327346074d16d9c0021d25134d4eeb9e
Jeff Webster <jwebster@navexglobal.com>
2017-09-28 16:05:44 -0700

    Add Fill-A-Pix Types project

---
+++ Fill-A-Pix Types/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Fill_A_Pix_Types.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Fill-A-Pix Types")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Fill-A-Pix Types")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("5afb8b2c-77e3-44fe-9439-a1fbdb9f5b45")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Fill-A-Pix Types/Messages.fs
@@ -0,0 +1,39 @@
+﻿namespace Fill_A_Pix_Types
+
+module Messages =
+
+    open GameTypes
+    open Akka.FSharp
+    open Akka.Actor
+
+    // Select a File
+    type OpenBitmapFileMsg = | OpenBitmapFile of BitmapFile
+    type ShowImageMsg = | ShowWindowImage of WindowBitmap
+                        | ShowBoardImage of BoardBitmap
+    type DetectBoardMsg = | DetectBoard of WindowBitmap
+
+    // Detect board
+    type DetectSidesMsg = | DetectSides of WindowBitmap
+    type FoundSideMsg = | FoundTopSide of PixelY
+                        | FoundBottomSide of PixelY
+                        | FoundLeftSide of PixelX
+                        | FoundRightSide of PixelX
+    type SidesFinalizedMsg = | SidesFinalized of PixelRect
+    type DetectCellsMsg = | DetectCells of BoardBitmap
+    type FoundCellsMsg = | FoundCells of (CoordX*PixelX*PixelX)[]*(CoordY*PixelY*PixelY)[]
+    type DetectClueMsg = | DetectClue of CoordX*CoordY*CellBitmap
+    type FoundClueMsg = | FoundClue of CoordX*CoordY*(Clue option)
+    type CluesFinalizedMsg = | CluesFinalized of Cell[,]
+    type SetBoardStateMsg = | SetBoardState of BoardState
+
+    // play game
+    type StepMoveMsg = | StepMove // to coordinator
+    type FoundMoveMsg = | BeginningOfGame
+                        | NothingFound
+                        | StartingClue of Cell*BoardState
+                        | EnoughFilled of Cell*BoardState
+                        | EnoughBlank of Cell*BoardState
+    type UpdateBoardStateMsg = | UpdateCellState of Cell*CellState
+                               | UpdateClueState of Cell*ClueState
+
+    type SetUiMsg = | SetUi of IActorRef
