e43e23da2cf22f65f1e44f19271db64cc54f7183
Jeff Webster <jwebster@navexglobal.com>
2017-11-01 21:15:40 -0700

    Add script files for MailboxProcessor and Class Actors

---
+++ Fill-A-Pix/AkkaClass.fsx
@@ -0,0 +1,22 @@
+﻿#r "C:\\git\\MySandbox\\Fill-A-Pix\\packages\\Akka.1.2.3\\lib\\net45\\Akka.dll"
+#r "C:\\git\\MySandbox\\Fill-A-Pix\\packages\\Akka.FSharp.1.2.3\\lib\\net45\\Akka.FSharp.dll"
+
+open Akka.Actor
+open Akka.FSharp
+
+type CounterActor() =
+    inherit UntypedActor()
+        override this.OnReceive (msg:obj) =
+            match msg with
+            | :? int as msg' ->
+                match msg' with
+                | x when x < 0 -> printfn "Neg int!"
+            printfn "Received message %A" msg               
+                        
+let system = System.create "system" <| Configuration.defaultConfig()
+                        
+// Use Props() to create actor from type definition
+let echoActor = system.ActorOf(Props(typedefof<CounterActor>), "echo")
+
+// tell a message
+echoActor <! "Hello World!"
